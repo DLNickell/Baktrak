119bb8428c110bacaf88211c75d0ffc2a8a58762
Dennis N <dnickell@protonmail.com>
2021-07-24 15:34:34 -0500

    begin explanation on code page

--- js/site.js
+++ js/site.js
@@ -3,30 +3,31 @@
 //Controller function
 function getValue(){
     document.getElementById("alert").classList.add("invisible");
-
     let userString = document.getElementById("userString").value;
-
     let revString = reverseString(userString);
-
-    displayString(revString);
-}
+    displayString(revString);}
 
 //Reverse the string
 //Logic function
 function reverseString(userString){
-
     let revString = [];
 
-    //Reverse a string using a for loop
-    for (let index = 0; index < array.length; index++) {
-        const element = array[index];
-        
+        //Reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];        
     }
+    return revString;
 
 }
 
 //Display the reversed string to the user
 //View function
-function displayString(){
+function displayString(revString){
+
+    //Write to the page
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+
+    //Show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 
 }
