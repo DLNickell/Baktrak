5944c36f15d7059d991de2174c6aa879dee43ef0
Dennis N <dnickell@protonmail.com>
2021-07-24 11:14:59 -0500

    Initial set up

--- js/site.js
+++ js/site.js
@@ -1,5 +1,32 @@
 
-//Call Hello World
-function helloWorld() {
-    alert("hello world");
+//Get the string from the page
+//Controller function
+function getValue(){
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+}
+
+//Reverse the string
+//Logic function
+function reverseString(userString){
+
+    let revString = [];
+
+    //Reverse a string using a for loop
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+
+}
+
+//Display the reversed string to the user
+//View function
+function displayString(){
+
 }
